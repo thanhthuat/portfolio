e79e3da8a8a04b7a7fa4ab394be13132b08d3152
thanhthuat <thanhthuat07@gmail.com>
2023-05-10 16:56:18 +0700

    add responsive header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,25 +1,55 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "public/image/logo.svg";
+import { useWindowDimensions } from "@hook/hooks";
 
 interface HeaderPageProps {
   className?: string;
 }
 
 const HeaderPage: React.FC<HeaderPageProps> = ({ className = "" }) => {
+  const [rotate, setRotate] = useState<boolean>(false);
+  const headerRef = useRef<HTMLDivElement>(null);
+  const navRef = useRef<HTMLDivElement>(null);
+  const size = useWindowDimensions();
+  const showNavbar = () => {
+    navRef?.current!.classList.toggle("responsive_nav");
+  };
+  useEffect(() => {
+    if (rotate) {
+      document.body.classList.add("disable-scrolling");
+    } else {
+      document.body.classList.remove("disable-scrolling");
+    }
+    return () => {};
+  }, [rotate]);
+  useEffect(() => {
+    if (size?.width! > 1100) {
+      setRotate(false);
+    }
+  }, [size]);
+
   return (
     <div className="clsheaderpage">
       <div className={`${className}  clsheaderpage-content`}>
-        <nav className="clsheaderpage-navbar wapperlayout">
+        <nav
+          className={
+            rotate
+              ? "clsheaderpage-navbar wapperlayout responsive_nav"
+              : " clsheaderpage-navbar wapperlayout "
+          }
+        >
           <div className="clsheaderpage-logo">
             <Link href={"/"}>
               <img src={"/image/logo.svg"} alt="logo"></img>
             </Link>
-
-            {/* <Image ></Image> */}
           </div>
-          <ul className="clsheaderpage-link">
+          <ul
+            className={
+              rotate ? "clsheaderpage-link active" : "clsheaderpage-link"
+            }
+          >
             <li className="clsheaderpage-link__item">
               <Link href={"/project"}> PROJECTS</Link>
             </li>
@@ -27,14 +57,20 @@ const HeaderPage: React.FC<HeaderPageProps> = ({ className = "" }) => {
               <Link href={"/services"}>SERVICES</Link>
             </li>
             <li className="clsheaderpage-link__item">
-              <Link href={"/services"}>SERVICES</Link>
-            </li>
-            <li className="clsheaderpage-link__item">
               <Link href={"/about"}>ABOUT</Link>
             </li>
+            <div className="clsheaderpage-button">
+              <Link href={"/contact"}>GEt in touch</Link>{" "}
+            </div>
           </ul>
-          <div className="clsheaderpage-button">
-            <Link href={"/contact"}>GEt in touch</Link>{" "}
+
+          <div
+            className={rotate ? "icon-burger active " : "icon-burger"}
+            onClick={() => setRotate(!rotate)}
+          >
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
           </div>
         </nav>
       </div>
